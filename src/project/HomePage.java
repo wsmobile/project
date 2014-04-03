b13fe4e6bcfdca0def8ec72bd72d1024cf739bc3
wsmobile <ntuniu@yahoo.com>
2014-04-03 13:28:18 -0400

    project commit1

--- src/project/HomePage.java
+++ src/project/HomePage.java
@@ -4,8 +4,15 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -13,47 +20,124 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class HomePage {
-	
 
+    public static void main(String[] args) {
 
-public static void main(String[] args) {
-        
         final JFrame frame = new JFrame("HomePage");
+        //frame.setLocationRelativeTo(null);
+        final int x = (Toolkit.getDefaultToolkit().getScreenSize().width/2);
+        final int y = (Toolkit.getDefaultToolkit().getScreenSize().height/2);
+        frame.setLocation(x-150, y-150);
+        
         JButton registerButton = new JButton("Register");
         JButton loginButton = new JButton("Log in");
+        final JTextField tempusername = new JTextField(10);
+        final JPasswordField temppassword = new JPasswordField(10);
         
- 
 
-        
-        JPanel panel = new JPanel(new GridLayout(3,2));
-    
+        JPanel panel = new JPanel(new GridLayout(3, 2));
+
         panel.add(new JLabel("Enter username:"));
-        panel.add(new JTextField(10));
+        panel.add(tempusername);
         panel.add(new JLabel("Enter password:"));
-        panel.add(new JTextField(10));
+        panel.add(temppassword);
         panel.add(registerButton);
         panel.add(loginButton);
-       
-		registerButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				JOptionPane.showMessageDialog(frame, "You won.");
-			}
-		});
+
+        registerButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                
+                File f = new File (tempusername.getText()+".txt");
+                if (f.exists())
+                    JOptionPane.showMessageDialog(frame, "Username exists!");
+                else{
+                try {
+                    BufferedWriter out = new BufferedWriter(new FileWriter(tempusername.getText()+".txt"));
+                    
+                        out.write(temppassword.getText() + "\n");
+                       
+                    
+                    out.close();
+                } catch (IOException ee) {ee.printStackTrace();}
+                }
+
+            }
+        });
         
-        
+           loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                File f = new File (tempusername.getText()+".txt");
+                
+                
+                if (tempusername.getText().length()==0){
+                    JOptionPane.showMessageDialog(frame, "Username was empty!\nPLease enter username.");
+                }
+                
+                else if (temppassword.getText().length()==0){
+                    JOptionPane.showMessageDialog(frame, "Password was empty!\nPLease enter apassword.");
+                }                
+                
+                else if (!f.exists())
+                    JOptionPane.showMessageDialog(frame, "Username does not exists!\nPlease register.");
+                
+                else if ( (tempusername.getText().equals("admin")) && (temppassword.getText().equals("pass")) ){
+                    //open in seller mode
+                    System.out.println("ad");
+                    Owner page=new Owner();
+                    page.setLocation(x-200, y-200);
+                    page.setVisible(true);
+                    frame.setVisible(false);
+                    
+                }
+                
+                else {
+                    BufferedReader br = null;
+ 
+		try 
+                {
+                    String sCurrentLine;
+                    String pass = temppassword.getText();
+ 
+                    br = new BufferedReader(new FileReader(tempusername.getText()+".txt"));
+ 
+                    while ((sCurrentLine = br.readLine()) != null) 
+                    {
+			if (sCurrentLine.equals(pass))
+                        {
+                            //open in buyermode
+                            System.out.println("ss");
+                        }
+                        
+                        else {
+                            JOptionPane.showMessageDialog(frame, "Password does not match!\nPlease try again!");
+                        }
+                   
+ 
+		} }
+                
+                catch (IOException eee) 
+                {
+                    eee.printStackTrace();
+		} 
+                }
+
+            }
+        });
+
         frame.add(panel, BorderLayout.CENTER);
-           
-        
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       
-       //frame.setSize(800, 700);
-       frame.pack();
-       frame.setVisible(true);
-        
+
+        //frame.setSize(800, 700);
+        frame.pack();
+        frame.setVisible(true);
+
     }
-	
+
 }
---
+++ src/project/Owner.java
@@ -0,0 +1,15 @@
+package project;
+
+import javax.swing.*;
+import java.awt.*;
+  
+class Owner extends JFrame
+ {
+   Owner()
+  {
+      
+  setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+  setTitle("Welcome");
+  setSize(400, 200);
+   }
+  } 
